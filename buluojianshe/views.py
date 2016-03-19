62d52ec53fd95a78d9f757d209250e2ea73084b2
benjaminfs <h1n1china@163.com>
2016-03-19 23:48:09 +0800

    2016.03.19

--- buluojianshe/views.py
+++ buluojianshe/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
 from models import Benjamin
+from django.template import RequestContext
 
 
 def buluojianshe_list(request):
     benjamins = Benjamin.objects.all().order_by("-id")
-    return render_to_response("buluojianshe_list.html", {"benjamins": benjamins})
+    return render_to_response("buluojianshe_list.html", {"benjamins": benjamins}, context_instance=RequestContext(request))

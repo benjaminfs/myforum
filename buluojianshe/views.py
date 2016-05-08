f1a4fad26d74cdbc482c1edfa96f89d77fe293fd
benjaminfs <h1n1china@163.com>
2016-05-08 22:37:37 +0800

    20160508

--- buluojianshe/views.py
+++ buluojianshe/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render_to_response
 from models import Benjamin
 from django.template import RequestContext
+from message.models import UserMessage
 
 
 def buluojianshe_list(request):
+    if request.user.is_authenticated():
+        msg_cnt = UserMessage.objects.filter(owner=request.user, status=0).count()
+    else:
+        msg_cnt = 0
     benjamins = Benjamin.objects.all().order_by("-id")
-    return render_to_response("buluojianshe_list.html", {"benjamins": benjamins}, context_instance=RequestContext(request))
+    return render_to_response("buluojianshe_list.html", {"benjamins": benjamins, "msg_cnt": msg_cnt}, context_instance=RequestContext(request))
